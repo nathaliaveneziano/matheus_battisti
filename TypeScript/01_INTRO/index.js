52c5be8e73f42b3ea58f32e14691d01565899438
Nathália Veneziano <nathaliaveneziano@icloud.com>
2023-08-14 13:38:51 -0300

    understanding fundamentals

--- TypeScript/01_INTRO/index.js
+++
@@ -1,9 +0,0 @@
-var firstName = 'Nathália';
-var anotherName = 1;
-var x = true;
-function greetings(name) {
-    console.log("Ol\u00E1 ".concat(name));
-}
-greetings(firstName);
-greetings(anotherName);
-greetings(x);
